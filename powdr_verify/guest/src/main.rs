dc1595880a39197f8edf06e94c33352e3805edd7
8ulce28 <kucukkerosist@gmail.com>
2025-08-06 17:08:53 +0300

    Adding Powdr

---
+++ powdr_verify/guest/src/main.rs
@@ -0,0 +1,27 @@
+use powdr_riscv_runtime;
+use k256::{
+    ecdsa::{Signature, VerifyingKey, signature::Verifier},
+    PublicKey,
+};
+
+fn main() {
+
+    // Hardcoded values for the sake of speed
+
+    let msg_bytes: [u8; 32] = [127, 208, 178, 212, 164, 94, 120, 175, 39, 225, 46, 45, 227, 107, 13, 145, 174, 219,
+    233,37, 221, 212, 183, 86, 115, 138, 76, 85, 204, 251, 36, 114];
+
+    let sig_bytes: [u8; 64] = [56, 88, 24, 56, 21, 204, 69, 193, 12, 201, 192, 190, 42, 154, 246, 131, 71, 178,
+    160, 96, 122, 247, 10, 91, 19, 16, 168, 66, 142, 117, 65, 54, 50, 62, 147, 140, 55, 85, 59, 106, 229, 60, 163,
+    88, 126, 250, 167, 92, 223, 122, 98, 145, 137, 29, 235, 204, 189, 68, 223, 173, 101, 150, 68, 118];
+
+    let pk_bytes: [u8; 65] = [4, 63, 52, 166, 212, 246, 164, 155, 160, 73, 190, 250, 87, 198, 253, 180, 36, 60, 27,
+    126, 62, 172, 250, 42, 150, 8, 76, 84, 193, 181, 77, 180, 211, 65, 200, 153, 158, 220, 21, 41, 198, 56, 122, 9,
+    238, 13, 107, 71, 90, 156, 219, 185, 44, 135, 251, 61, 100, 163, 1, 234, 181, 49, 78, 75, 185];
+
+    let signature = Signature::from_bytes((&sig_bytes).into()).expect("Invalid signature format");
+    let public_key = PublicKey::from_sec1_bytes(&pk_bytes).expect("Invalid public key");
+    let verify_key = VerifyingKey::from(public_key);
+
+    assert_eq!(verify_key.verify(&msg_bytes, &signature).is_ok(), true);
+}
---
+++ powdr_verify/src/main.rs
@@ -0,0 +1,33 @@
+use powdr::Session;
+use std::{fs, path::PathBuf};
+
+fn main() {
+    env_logger::init();
+
+    // Initiating the Powdr session
+    let mut session = Session::builder()
+        .guest_path("./guest")
+        .out_path("powdr-target")
+        .chunk_size_log2(20)  // The default value is 20 but can be changed to 18, 19
+        .build();
+
+    session.run();
+
+    session.prove();
+
+    // Powdr creates chunks to generate proof
+    let chunk_num = 5;
+    let mut total_size = 0;
+
+    // Extracting proof size
+    for i in 0..chunk_num {
+        let mut proof_path = PathBuf::from("./powdr-target");
+        proof_path.push(format!("chunk_{}", i));
+        proof_path.push("guest_proof.bin");
+
+        let bytes = fs::read(&proof_path).unwrap();
+        total_size += bytes.len();
+    }
+
+    println!("Total proof size: {} bytes", total_size);
+}
