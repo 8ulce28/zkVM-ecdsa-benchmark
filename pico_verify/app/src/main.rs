c34bdf279a75e02452f2ed0abd34da8581043598
8ulce28 <kucukkerosist@gmail.com>
2025-08-07 14:21:27 +0300

    Update main.rs

--- pico_verify/app/src/main.rs
+++ pico_verify/app/src/main.rs
@@ -1,20 +1,17 @@
 #![no_main]
 pico_sdk::entrypoint!(main);
 
-use secp256k1::{
+use k256::{
+    ecdsa::{Signature, VerifyingKey, signature::Verifier},
     PublicKey,
-    ecdsa::{RecoverableSignature as Secp256k1RecoverableSignature, RecoveryId, Signature},
-    Message,
 };
 
-pub fn main() {
+fn main() {
 
     // Feeding hardcoded values for the sake of speed
 
-    let secp = secp256k1::Secp256k1::new();
-
-    let message_hash: [u8; 32] = [127, 208, 178, 212, 164, 94, 120, 175, 39, 225, 46, 45, 227, 107, 13, 145,
-    174, 219, 233, 37, 221, 212, 183, 86, 115, 138, 76, 85, 204, 251, 36, 114];
+    let msg_bytes: [u8; 32] = [127, 208, 178, 212, 164, 94, 120, 175, 39, 225, 46, 45, 227, 107, 13, 145, 174,
+    219, 233, 37, 221, 212, 183, 86, 115, 138, 76, 85, 204, 251, 36, 114];
 
     let sig_bytes: [u8; 64] = [56, 88, 24, 56, 21, 204, 69, 193, 12, 201, 192, 190, 42, 154, 246, 131, 71, 178,
     160, 96, 122, 247, 10, 91, 19, 16, 168, 66, 142, 117, 65, 54, 50, 62, 147, 140, 55, 85, 59, 106, 229, 60, 163,
@@ -24,24 +21,11 @@ pub fn main() {
     126, 62, 172, 250, 42, 150, 8, 76, 84, 193, 181, 77, 180, 211, 65, 200, 153, 158, 220, 21, 41, 198, 56, 122, 9,
     238, 13, 107, 71, 90, 156, 219, 185, 44, 135, 251, 61, 100, 163, 1, 234, 181, 49, 78, 75, 185];
 
-    let pk = PublicKey::from_slice(&pk_bytes)
-        .expect("Public key cannot be created from bytes");
+    let signature = Signature::from_bytes((&sig_bytes).into()).expect("Invalid signature format");
+    let public_key = PublicKey::from_sec1_bytes(&pk_bytes).expect("Invalid public key");
+    let verify_key = VerifyingKey::from(public_key);
 
-    let message = Message::from_digest_slice(&message_hash)
-        .expect("Message could not be created from bytes");
-
-    let recovery_id = RecoveryId::from_i32(1).unwrap();
-
-    let signature = Secp256k1RecoverableSignature::from_compact(&sig_bytes, recovery_id).unwrap();
-
-    let public_key = secp.recover_ecdsa(&message, &signature)
-        .expect("Public key cannot be recovered");
-
-    let sig = Signature::from_compact(&sig_bytes).unwrap();
-
-    let result = secp.verify_ecdsa(&message, &sig, &public_key);
-
-    assert!(result.is_ok());
-
-    assert_eq!(public_key, pk);  // Checking the equivalence of expected and extracted pk
+    verify_key
+        .verify(&msg_bytes, &signature)
+        .expect("Signature failed to verify");
 }
